cb03d5586995d5e1cf63ed02b4e80b5bd4f73c66
poojabohora <poojab@winjit.com>
2018-07-25 16:52:15 +0530

    added code for testing

--- Autobuild/ViewController.swift
+++ Autobuild/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet let lbl : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("")
+        //print("abc")
+        lbl.text = "Dasdsd"
+        
     }
 
     override func didReceiveMemoryWarning() {
